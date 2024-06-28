8bf3239bc7f450e15fe298d0c5be6df86918612a
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-06-28 21:54:19 +0700

    Added functions to remove books and toggle between `read` and `not read`, cleaned some code by moving duplicates into functions, and redid some CSS.

--- library.js
+++ library.js
@@ -15,32 +15,107 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-const lotr = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 600, 'not read yet');
-const silmarillion = new Book('The Silmarillion', 'J.R.R. Tolkien', 400, 'not read yet');
+// Demo data
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, 'not read');
+const lotr = new Book('The Lord of the Rings (single-volume)', 'J.R.R. Tolkien', 1077, 'read');
+const silmarillion = new Book('The Silmarillion', 'J.R.R. Tolkien', 365, 'not read');
 
-addBookToLibrary(theHobbit);
+addBookToLibrary(hobbit);
 addBookToLibrary(lotr);
 addBookToLibrary(silmarillion);
 
 function showBooksInLibrary() {
-    myLibrary.forEach((each, index) => {
-        const aBook = document.createElement('div');
-        aBook.setAttribute('class', 'book');
-        aBook.setAttribute('id', index);
-        aBook.innerHTML = each.info();
-        bookList.appendChild(aBook);
+    myLibrary.forEach((each, index) => bookList.appendChild(createBook(each, index)));
+}
 
-        const remove = document.createElement('button');
-        remove.setAttribute('id', index);
-        remove.innerHTML = 'x';
-        aBook.appendChild(remove);
-    });
+function createBook(each, index) {
+    const box = document.createElement('div');
+    const book = document.createElement('div');
+    const selections = document.createElement('div');
+
+    box.setAttribute('class', 'box');
+
+    book.setAttribute('class', 'book');
+    book.setAttribute('id', index);
+    book.innerHTML = each.info();
+
+    selections.setAttribute('class', 'selections');
+    selections.appendChild(createRemoveButton(index));
+    selections.appendChild(createReadToggle(each, index));
+
+    box.appendChild(book);
+    box.appendChild(selections);
+
+    return box; // Return to function in showBooksInLibrary()
+}
+
+function createRemoveButton(index) {
+    const removeButton = document.createElement('button');
+
+    removeButton.setAttribute('id', index);
+    removeButton.innerHTML = 'x';
+    removeButton.addEventListener('click', removeBook.bind(this, removeButton.id));
+
+    return removeButton; // Return to function in createBook(each, index)
+}
+
+function removeBook(bookID) {
+    myLibrary.splice(bookID, 1); // Remove one element starting at bookID
+    restartLibrary();
+}
+
+function createReadToggle(each, index) {
+    const returnSelection = document.createElement('span');
+    const readToggle = document.createElement('input');
+    const readLabel = document.createElement('label');
+    const notReadToggle = document.createElement('input');
+    const notReadLabel = document.createElement('label');
+    
+    readToggle.setAttribute('type', 'radio');
+    readToggle.setAttribute('id', 'read');
+    readToggle.setAttribute('name', index);
+    readToggle.setAttribute('value', 'read');
+    readToggle.addEventListener('click', toggleRead.bind(this, readToggle, index));
+    
+    readLabel.setAttribute('for', 'read');
+    readLabel.innerHTML = 'Read';
+    
+    notReadToggle.setAttribute('type', 'radio');
+    notReadToggle.setAttribute('id', 'not-read');
+    notReadToggle.setAttribute('name', index);
+    notReadToggle.setAttribute('value', 'not read');
+    notReadToggle.addEventListener('click', toggleRead.bind(this, notReadToggle, index));
+    
+    notReadLabel.setAttribute('for', 'not-read');
+    notReadLabel.innerHTML = 'Not read';
+
+    if (each.read == 'read') {
+        readToggle.setAttribute('checked', 'true');
+    } else {
+        notReadToggle.setAttribute('checked', 'true');
+    }
+
+    returnSelection.appendChild(readToggle);
+    returnSelection.appendChild(readLabel);
+    returnSelection.appendChild(notReadToggle);
+    returnSelection.appendChild(notReadLabel);
+    
+    return returnSelection; // Return to function in createBook(each, index)
+}
+
+function toggleRead(toggle, index) {
+    myLibrary[index].read = toggle.id;
+    restartLibrary();
+}
+
+function restartLibrary() {
+    bookList.innerHTML = '';
+    showBooksInLibrary();
 }
 
 const bookList = document.querySelector('#container');
-const newBookButton = document.querySelector('#new-book');
 const dialog = document.querySelector('dialog');
+const newBookButton = document.querySelector('#new-book');
 const form = document.querySelector('#form');
 const closeButton = document.querySelector('#close');
 
@@ -58,16 +133,8 @@ function submitForm(event) {
 
     addBookToLibrary(new Book(title.value, author.value, pages.value, readYet.value));
     dialog.close();
-    
-    const aBook = document.createElement('div');
-    aBook.setAttribute('class', 'book');
-    aBook.setAttribute('id', myLibrary.length - 1);
-    aBook.innerHTML = myLibrary.slice(-1)[0].info(); // Slice the last element of myLibrary, select index 0 (the only element), and use the info() method.
-    bookList.appendChild(aBook);
-
-    const remove = document.createElement('button');
-    remove.innerHTML = 'x';
-    aBook.appendChild(remove);
-
+    bookList.appendChild(createBook(myLibrary[myLibrary.length - 1], myLibrary.length - 1));
+    // myLibrary[myLibrary.length - 1] -> Access the just-added Book object (last element in myLibrary)
+    // myLibrary.length - 1 -> Access the index of last element in myLibrary
     event.preventDefault();
 }
